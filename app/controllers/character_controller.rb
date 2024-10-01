0ab4e5dd61ff808de24f816c479f3afa5d4fe271
JoelDurafourd <joel.durafourd@gmail.com>
2024-10-01 16:13:20 +0900

    added controller options and other things, added location to chatacter class

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,2 +1,22 @@
 class CharacterController < ApplicationController
+
+  def new
+    @user = current_user
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(user: current_user, name: @character, health: 100, hunger: 100, attack:, 1, location: "starting")
+    if @note.save
+      redirect_to user_character_notes_path(current_user, @character), notice: 'Character was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:user_id, :name, :health, :hunger, :attack, :location)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
       sessions: 'users/sessions'
     }
   root to: "pages#home"
+
+  resources :users, only: [:show, :index] do
+    resources :characters, only: [:new, :show, :index]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
