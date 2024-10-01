e467532999713da6bc862358084bd119d81441a8
JoelDurafourd <joel.durafourd@gmail.com>
2024-10-01 17:20:44 +0900

    created some interesting bugs

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -15,7 +15,7 @@ class CharactersController < ApplicationController
   end
 
   def create
-    @character = Character.new(user: current_user, name: @character, health: 100, hunger: 100, attack: 1, location: "starting")
+    @character = Character.new(user_id: current_user, name: @character, health: 100, hunger: 100, attack: 1, location: "starting")
     if @note.save
       redirect_to user_character_notes_path(current_user, @character), notice: 'Character was successfully created.'
     else
