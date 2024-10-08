e481072e57aac0b51e2a80d948a984ce04886dd4
JoelDurafourd <joel.durafourd@gmail.com>
2024-10-08 16:46:38 +0900

    got the new locations button to function, now I just need it to delete the location

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  resources :locations, only: [:new, :create]
   # resources :locations, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
